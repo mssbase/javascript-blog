22d0f0062480a9781de0f1a6e5e38457c1b7e45c
Michal S <michalsienczuk@gmail.com>
2020-11-17 21:21:01 +0100

    add all exercises

--- js/script.js
+++ js/script.js
@@ -19,8 +19,6 @@ console.log(activeLinks)
     }
     /* add class 'active' to the clicked link */
     // console.log('clickedElement:', clickedElement);
-
-    // const Articles = document.querySelectorAll('.titles a');
         clickedElement.classList.add('active');
     console.log('clickedElement (with plus): ' + clickedElement);
 
@@ -32,10 +30,16 @@ console.log(activeArticles)
     }
 
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
 
+console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+    
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active')
+    console.log(targetArticle);
 }
 // console.log(titleClickHandler());
 
